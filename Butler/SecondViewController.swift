91d2a5c355f3dfbe5169ecf09103fef3960039af
Max Kramer <max@maxkramer.co>
2016-05-16 10:40:57 +0100

    Initial Commit

---
+++ Butler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Butler
+//
+//  Created by Max Kramer on 16/05/2016.
+//  Copyright © 2016 Max Kramer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
