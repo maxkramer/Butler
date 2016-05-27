ad6819bc6a98f802bbe38b907bcd37c1a00585df
Max Kramer <max@maxkramer.co>
2016-05-27 17:14:57 +0100

    Using R to instantiate the nib.

--- Butler/Coordinators/Send Request/SendRequestTabCoordinator.swift
+++ Butler/Coordinators/Send Request/SendRequestTabCoordinator.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 
-class SendRequestTabCoordinator: TabCoordinator {
+class SendRequestTabCoordinator: TabCoordinator, SendRequestViewControllerDelegate {
     lazy var rootViewController: UIViewController = {
-        let rvc = SendRequestViewController(nibName: "SendRequestViewController", bundle: NSBundle.mainBundle())
+        let rvcNib = R.nib.sendRequestViewController
+        
+        let rvc = SendRequestViewController(nibName: rvcNib.name, bundle: rvcNib.bundle)
         rvc.tabBarItem = self.tabBarItem
         rvc.navigationItem.title = R.string.localizable.sendrequestTitle()
+        rvc.delegate = self
         return rvc
     }()
     
     var tabBarItem = UITabBarItem(image: R.image.sendRequestTbi, selectedImage: R.image.sendRequestTbiSelected, topInset: 5)
     
     func start() {}
+    
+    // MARK: SendRequestViewControllerDelegate
+    
+    func sendRequestViewController(sendRequestViewController: SendRequestViewController, didSendRequestSuccessfully response: Response) {
+        Cerberus.info("Request sent successfully. Received response: \(response)")
+    }
 }
